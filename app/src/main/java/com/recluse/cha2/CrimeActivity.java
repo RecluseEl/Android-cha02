cd025895f4a5bf51fbf760f84485ef7bdea85b82
recluseel <815556108@qq.com>
2019-12-04 12:36:05 +0800

    1.fragment 2.fragmengt Manager in CrimeActivity

--- app/src/main/java/com/recluse/cha2/CrimeActivity.java
+++ app/src/main/java/com/recluse/cha2/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.recluse.cha2;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //管理Fragment，把他们的视图增加到视图层级结构
+        FragmentManager fm=getSupportFragmentManager();
+        Fragment fragment=fm.findFragmentById(R.id.fragment_container);
+        if(fragment==null){
+            fragment=new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+
     }
 }
