a2d0bf2e2648e3c7e6a289be997e80266f31a081
recluseel <815556108@qq.com>
2019-12-05 22:17:04 +0800

    1.fragment

---
+++ app/src/main/java/com/recluse/cha2/SingleFragmentActivity.java
@@ -0,0 +1,26 @@
+package com.recluse.cha2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        //管理Fragment，把他们的视图增加到视图层级结构
+        FragmentManager fm=getSupportFragmentManager();
+        //从队列里找到目标Fragment
+        Fragment fragment=fm.findFragmentById(R.id.fragment_container);
+        //比如翻转屏幕，Fragment会被销毁，FragmentManager会保留Fragment队列，这时应新建Fragment增加到原有位置。
+        if(fragment==null){
+            fragment=createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+
+    }
+}
