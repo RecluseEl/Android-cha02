e4278ab77a07cdbfc40d5c6b651f0591bfef47c5
recluseel <815556108@qq.com>
2019-12-18 22:30:20 +0800

    1.RecycleView demo 2.ListRow is a util class,the program also can run without this class

--- app/src/main/java/com/recluse/cha2/ListRow.java
+++ app/src/main/java/com/recluse/cha2/ListRow.java
@@ -4,18 +4,21 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 /*
-* ViewHolder的工具类，用于容纳View视图
+* ViewHolder的工具类，用于容纳View视图ad
 *
 *
 * */
+public class ListRow{}
+/*
 public class ListRow extends RecyclerView.ViewHolder{
     public ImageView mThumbnail;
 
     public ListRow(View view) {
         super(view);
-        mThumbnail=(ImageView)view.findViewById(R.id.thumbnail);
+        mThumbnail=(ImageView)view.findViewById(R.id.th);
 
 
     }
 
 }
+*/
